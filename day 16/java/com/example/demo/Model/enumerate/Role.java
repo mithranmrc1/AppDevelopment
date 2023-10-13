b48c6e353c82aaa4e785c1eb0dc67abb6a818e02
mithranmrc <144990574+mithranmrc1@users.noreply.github.com>
2023-10-13 10:12:21 +0530

    Add files via upload

---
+++ day 16/java/com/example/demo/Model/enumerate/Role.java
@@ -0,0 +1,14 @@
+package com.example.demo.Model.enumerate;
+
+import lombok.Getter;
+
+
+@Getter
+
+public enum Role 
+{
+	USER,
+	
+	ADMIN
+
+}
---
+++ day 16/java/com/example/demo/Noisetracker1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Noisetracker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Noisetracker1Application.class, args);
+	}
+
+}
