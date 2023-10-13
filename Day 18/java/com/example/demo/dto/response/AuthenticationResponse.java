406beab498327c616ebec3f9c04db294f19e4712
mithranmrc <144990574+mithranmrc1@users.noreply.github.com>
2023-10-13 10:10:41 +0530

    Add files via upload

---
+++ Day 18/java/com/example/demo/dto/response/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto.response;
+
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse 
+{
+	private String token;
+
+}
